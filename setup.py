91bc0e58c5fed41c874d8e2e7fd322c60d5336f3
MartinKliment21 <139995968+MartinKliment21@users.noreply.github.com>
2023-07-19 18:11:56 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='Mastermind',
+      version='1.0',
+      description='Python Simulates The Game Mastermind',
+      author='Martin Kliment',
+      author_email='martin.kliment@gmail.com',
+      packages=['mastermind'],
+     )
